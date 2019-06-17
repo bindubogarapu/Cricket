6ca98cb430952c344d4c09a799b096614c0e10dc
nithin2889 <nithin2889@gmail.com>
2019-06-17 03:50:16 -0700

    initial commit

--- src/main/java/com/cricket/dto/PlayerDTO.java
+++ src/main/java/com/cricket/dto/PlayerDTO.java
@@ -1,5 +1,30 @@
 package com.cricket.dto;
 
-public class PlayerDTO {
 
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+
+@Getter @Setter @ToString @NoArgsConstructor
+public class PlayerDTO {
+	private String playerName;
+	private int age;
+	private int role;
+	private String country;
+	private int style;
+	private long totalRuns;
+	private long totalMatches;
+	private long totalWickets;
+	private double strikeRate;
+	private double bowlingAvg;
+	private long centuries;
+	private long halfCenturies;
+	private String imagePath;
+	private String imageName;
+	
+	
+	
 }
--- src/main/java/com/cricket/entity/PlayerEntity.java
+++ src/main/java/com/cricket/entity/PlayerEntity.java
@@ -1,5 +1,29 @@
 package com.cricket.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Table(name = "player")
+
+@Getter @Setter @ToString @NoArgsConstructor
 public class PlayerEntity {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long playerId;
+	private String playerName;
+	private int age;
+	private int role;
+	private String country;
+	private int style;
 
 }
