728cd2c617bb5a40eba740130dbe5fd143ca9615
nithin2889 <nithin2889@gmail.com>
2019-06-17 03:48:24 -0700

    initial commit

--- src/main/java/com/cricket/entity/ProfileEntity.java
+++ src/main/java/com/cricket/entity/ProfileEntity.java
@@ -1,5 +1,38 @@
 package com.cricket.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Table(name = "player_profile")
+
+@Getter @Setter @ToString @NoArgsConstructor
 public class ProfileEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long profileId;	
+	@OneToOne
+	@JoinColumn(name = "player_id")
+	private PlayerEntity playerEntity;
+	private long totalRuns;
+	private long totalMatches;
+	private long totalWickets;
+	private double strikeRate;
+	private double bowlingAvg;
+	private long centuries;
+	private long halfCenturies;
+	private String imagePath;
+	private String imageName;
 
 }
